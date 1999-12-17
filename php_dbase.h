a64a74f029fcbea8ac51762aaa85ad02481bf587
Zeev Suraski <zeev@php.net>
1999-12-17 20:54:52 +0000

    - Made PHP_VERSION and PHP_OS work again - More php3_ cleanup @- Restored the PHP_VERSION and PHP_OS constants (Zeev)
    git-svn-id: http://svn.php.net/repository/pecl/dbase/trunk@17380 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_dbase.h
+++ php_dbase.h
@@ -27,12 +27,12 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_dbase.h,v 1.2 1999/05/21 10:05:47 sas Exp $ */
+/* $Id: php_dbase.h,v 1.3 1999/07/27 17:09:35 andrey Exp $ */
 
 #ifndef _DBASE_H
 #define _DBASE_H
 #if DBASE
-extern php3_module_entry dbase_module_entry;
+extern zend_module_entry dbase_module_entry;
 #define dbase_module_ptr &dbase_module_entry
 
 extern PHP_MINIT_FUNCTION(dbase);
