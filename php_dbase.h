69eb3845774587e5caddad76be19521c98a20da0
Sebastian Bergmann <sebastian@php.net>
2002-12-31 16:34:27 +0000

    MFH: Bump year.
    git-svn-id: http://svn.php.net/repository/pecl/dbase/branches/PHP_4_3@109598 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_dbase.h
+++ php_dbase.h
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | PHP Version 4                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2002 The PHP Group                                |
+   | Copyright (c) 1997-2003 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 2.02 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_dbase.h,v 1.9 2001/12/11 15:29:04 sebastian Exp $ */
+/* $Id: php_dbase.h,v 1.10 2002/02/28 08:26:01 sebastian Exp $ */
 
 #ifndef PHP_DBASE_H
 #define PHP_DBASE_H
