c873b206f3bcb8f18485293211d6ad6e683a8bd5
Zak Greant <zak@php.net>
2003-07-15 18:19:52 +0000

    Added function dbase_get_header_info  * Passed a handle for an open dbase database, it returns an array of    associative arrays. Each associative array contains information on a    single column, including name, column type, length, precision, ...
    git-svn-id: http://svn.php.net/repository/pecl/dbase/trunk@135130 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_dbase.h
+++ php_dbase.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_dbase.h,v 1.11 2002/12/31 16:06:26 sebastian Exp $ */
+/* $Id: php_dbase.h,v 1.12 2003/06/10 20:03:27 imajes Exp $ */
 
 #ifndef PHP_DBASE_H
 #define PHP_DBASE_H
@@ -35,6 +35,7 @@ PHP_FUNCTION(dbase_get_record);
 PHP_FUNCTION(dbase_delete_record);
 PHP_FUNCTION(dbase_pack);
 PHP_FUNCTION(dbase_get_record_with_names);
+PHP_FUNCTION(dbase_get_header_info);
 #else
 #define dbase_module_ptr NULL
 #endif
