a4f503db77a142da4b290ed5ef578d6bb332e799
Andi Gutmans <andi@php.net>
2004-01-08 08:15:07 +0000

    - A belated happy holidays and PHP 5
    git-svn-id: http://svn.php.net/repository/pecl/dbase/trunk@148150 c90b9560-bf6c-de11-be94-00142212c4b1

--- dbase.c
+++ dbase.c
@@ -1,8 +1,8 @@
 /*
    +----------------------------------------------------------------------+
-   | PHP Version 4                                                        |
+   | PHP Version 5                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2003 The PHP Group                                |
+   | Copyright (c) 1997-2004 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.0 of the PHP license,       |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: dbase.c,v 1.70 2003/10/14 07:49:34 steinm Exp $ */
+/* $Id: dbase.c,v 1.71 2003/11/27 11:32:05 steinm Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
