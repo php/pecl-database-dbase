257b3002935d4ff108a10e9026f65a9aa2aae7d8
foobar <sniper@php.net>
2005-08-03 14:06:58 +0000

    - Bumber up year
    git-svn-id: http://svn.php.net/repository/pecl/dbase/trunk@192188 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_dbase.h
+++ php_dbase.h
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | PHP Version 5                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2004 The PHP Group                                |
+   | Copyright (c) 1997-2005 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.0 of the PHP license,       |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: php_dbase.h,v 1.13 2003/07/15 18:19:51 zak Exp $ */
+/* $Id: php_dbase.h,v 1.14 2004/01/08 17:32:00 sniper Exp $ */
 
 #ifndef PHP_DBASE_H
 #define PHP_DBASE_H
