56faa43e4b7c92a419aa07940591ae95c810a7f9
Hannes Magnusson <bjori@php.net>
2006-06-15 15:31:55 +0000

    MFH: Revert array type hinting
    git-svn-id: http://svn.php.net/repository/pecl/dbase/branches/PHP_5_2@214840 c90b9560-bf6c-de11-be94-00142212c4b1

--- dbase.c
+++ dbase.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: dbase.c,v 1.74.2.2 2006/01/01 12:50:05 sniper Exp $ */
+/* $Id: dbase.c,v 1.74.2.2.2.1 2006/06/11 01:42:17 bjori Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -758,13 +758,13 @@ ZEND_END_ARG_INFO()
 static
 ZEND_BEGIN_ARG_INFO(arginfo_dbase_add_record, 0)
 	ZEND_ARG_INFO(0, identifier)
-	ZEND_ARG_ARRAY_INFO(0, data, 0)
+	ZEND_ARG_INFO(0, data) /* ARRAY_INFO(0, data, 0) */
 ZEND_END_ARG_INFO()
 
 static
 ZEND_BEGIN_ARG_INFO(arginfo_dbase_replace_record, 0)
 	ZEND_ARG_INFO(0, identifier)
-	ZEND_ARG_ARRAY_INFO(0, data, 0)
+	ZEND_ARG_INFO(0, data) /* ARRAY_INFO(0, data, 0) */
 	ZEND_ARG_INFO(0, recnum)
 ZEND_END_ARG_INFO()
 
@@ -789,7 +789,7 @@ ZEND_END_ARG_INFO()
 static
 ZEND_BEGIN_ARG_INFO(arginfo_dbase_create, 0)
 	ZEND_ARG_INFO(0, filename)
-	ZEND_ARG_ARRAY_INFO(0, fields, 0)
+	ZEND_ARG_INFO(0, fields) /* ARRAY_INFO(0, fields, 0) */
 ZEND_END_ARG_INFO()
 
 static
