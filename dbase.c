d919f521e90bda9267cfd1c644293fdddf34ca7b
Ilia Alshanetsky <iliaa@php.net>
2006-11-22 17:30:35 +0000

    Fixed a possible crash in dbase_open() on empty filename.
    git-svn-id: http://svn.php.net/repository/pecl/dbase/branches/PHP_5_2@223737 c90b9560-bf6c-de11-be94-00142212c4b1

--- dbase.c
+++ dbase.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: dbase.c,v 1.74.2.2.2.4 2006/08/08 15:55:27 mike Exp $ */
+/* $Id: dbase.c,v 1.74.2.2.2.5 2006/10/10 23:01:23 tony2001 Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -128,6 +128,11 @@ PHP_FUNCTION(dbase_open)
 	}
 	convert_to_string_ex(dbf_name);
 	convert_to_long_ex(options);
+
+	if (!Z_STRLEN_PP(dbf_name)) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "The filename cannot be empty.");
+		RETURN_FALSE;
+	}
 
 	if (Z_LVAL_PP(options) == 1) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Cannot open %s in write-only mode", Z_STRVAL_PP(dbf_name));
