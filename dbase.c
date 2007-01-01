b6ad5189d23c359036d918edc3126db1cba7a95a
Sebastian Bergmann <sebastian@php.net>
2007-01-01 09:36:00 +0000

    MFH: Bump year.
    git-svn-id: http://svn.php.net/repository/pecl/dbase/branches/PHP_5_2@226100 c90b9560-bf6c-de11-be94-00142212c4b1

--- dbase.c
+++ dbase.c
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | PHP Version 5                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2006 The PHP Group                                |
+   | Copyright (c) 1997-2007 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.01 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: dbase.c,v 1.74.2.2.2.6 2006/11/22 17:30:34 iliaa Exp $ */
+/* $Id: dbase.c,v 1.74.2.2.2.7 2006/12/01 19:10:49 iliaa Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
