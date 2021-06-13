2f11d687a58d90fa84f910a92ac383013166f767
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-13 23:15:07 +0200

    Release dbase 7.1.0

--- php_dbase.h
+++ php_dbase.h
@@ -42,6 +42,6 @@ PHP_FUNCTION(dbase_get_header_info);
 
 #define phpext_dbase_ptr dbase_module_ptr
 
-#define PHP_DBASE_VERSION "7.1.0RC2"
+#define PHP_DBASE_VERSION "7.1.0"
 
 #endif /* PHP_DBASE_H */
